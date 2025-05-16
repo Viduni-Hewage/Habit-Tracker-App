806e048e03eaa42a31d2b7b46942df2968e49d44
Viduni-Hewage <viduhewage02@gmail.com>
2025-05-16 08:11:52 +0530

    modify the navigation with onboarding screens

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -4,6 +4,9 @@ import SplashScreen from "../screens/Splash.screen";
 import WelcomeScreen from "../screens/Welcome.screen";
 import SignInScreen from "../screens/SignIn.screen";
 import SignUpScreen from "../screens/SignUp.screen";
+import Onboarding1Screen from "../screens/OnBoard1.screen";
+import Onboarding2Screen from "../screens/OnBoard2.screen";
+import Onboarding3Screen from "../screens/OnBoard3.screen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ const AppNavigation = () => {
       <Stack.Screen name = "Welcome" component={WelcomeScreen} options = {{headerShown: false}}/>
       <Stack.Screen name = "Signin" component={SignInScreen} options = {{headerShown: false}}/>
       <Stack.Screen name = "Signup" component={SignUpScreen} options = {{headerShown: false}}/>
+      <Stack.Screen name = "Ad1" component={Onboarding1Screen} options = {{headerShown: false}}/>
+      <Stack.Screen name = "Ad2" component={Onboarding2Screen} options = {{headerShown: false}}/>
+      <Stack.Screen name = "Ad3" component={Onboarding3Screen} options = {{headerShown: false}}/>
     </Stack.Navigator>
   );
 };
